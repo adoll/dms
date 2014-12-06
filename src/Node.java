14972618861127273f5c224419e95e1b65d135db
Aaron Doll <dollaaron@gmail.com>
2014-12-05 19:02:36 -0500

    .gitignore, some crypto functionality

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,18 @@
+import java.io.File;
+
 public class Node {
-    
+        
     public String name;
     public String address;
+    
+    /**
+       Encrypts a file with a random key k, encrypts that file with the key,
+       splits k into n shares, of which m are sufficient for reconstructing
+       the key. Contacts the server, obtains a list of participating nodes,
+       and chooses $n$ random servers to distribute the key shares to. */
+    public File encryptAndSendKey(int m, int n, File input) {
+        
+
+    }
 
 }
